06255b91037e956a9a8eeb18395f1f185747b580
Daniel <dlu0@sunrise.ch>
2025-07-30 20:54:43 +0200

    Add files via upload
    - Added GPIO 12 & 13
    - Added servo commands

--- usb_gpio_arduino/usb_gpio_arduino.ino
+++ usb_gpio_arduino/usb_gpio_arduino.ino
@@ -1,12 +1,67 @@
+#include <Servo.h>
+
+#define MAX_SERVOS 8
+const int servo_pins[MAX_SERVOS] = {2, 3, 4, 5, 6, 7, 8, 9};
+Servo servos[MAX_SERVOS];
+bool servo_attached[MAX_SERVOS] = {false, false, false, false, false, false, false, false};
+
+int get_servo_index(int pin) {
+  for (int i = 0; i < MAX_SERVOS; i++) {
+    if (servo_pins[i] == pin) return i;
+  }
+  return -1;
+}
+
 void setup() {
-	Serial.begin(115200);
+  Serial.begin(115200);
 }
 
 void loop() {
-	if(Serial.available() > 0) {
-		char data = Serial.read();
-		
-    if (data == 'a') {
+  if (Serial.available() > 0) {
+    char data = Serial.read();
+
+    // --- SERVO COMMANDS ---
+    if (data == 'A') { // Attach Servo
+      while (!Serial.available());
+      int pin = Serial.read() - '0';
+      int idx = get_servo_index(pin);
+      if (idx != -1 && !servo_attached[idx]) {
+        servos[idx].attach(pin);
+        servo_attached[idx] = true;
+      }
+    }
+    else if (data == 'B') { // Servo auf Winkel stellen
+      while (Serial.available() < 2); // Warten auf Pin und mindestens 1 Ziffer
+      int pin = Serial.read() - '0';
+      int idx = get_servo_index(pin);
+      char winkel_str[4] = {0};
+      int i = 0;
+      // Bis zu 3 Ziffern für Winkel einlesen (z.B. 180)
+      while (i < 3 && Serial.available()) {
+        char c = Serial.peek();
+        if (c >= '0' && c <= '9') {
+          winkel_str[i++] = Serial.read();
+        } else {
+          break;
+        }
+      }
+      int winkel = atoi(winkel_str);
+      if (idx != -1 && servo_attached[idx]) {
+        servos[idx].write(winkel);
+      }
+    }
+    else if (data == 'C') { // Detach Servo
+      while (!Serial.available());
+      int pin = Serial.read() - '0';
+      int idx = get_servo_index(pin);
+      if (idx != -1 && servo_attached[idx]) {
+        servos[idx].detach();
+        servo_attached[idx] = false;
+      }
+    }
+    // --- END SERVO COMMANDS ---
+
+    else if (data == 'a') {
       pinMode(2, INPUT);
     } else if (data == 'b') {
       pinMode(3, INPUT);
@@ -26,6 +81,10 @@ void loop() {
       pinMode(10, INPUT);
     } else if (data == 'j') {
       pinMode(11, INPUT);
+    } else if (data == 'N') {
+      pinMode(12, INPUT);
+    } else if (data == 'I') {
+      pinMode(13, INPUT);
     }
 
     else if (data == 'k') {
@@ -48,6 +107,10 @@ void loop() {
       pinMode(10, OUTPUT);
     } else if (data == 't') {
       pinMode(11, OUTPUT);
+    } else if (data == 'O') {
+      pinMode(12, OUTPUT);
+    } else if (data == 'L') {
+      pinMode(13, OUTPUT);
     }
 
     else if (data == 'u') {
@@ -70,6 +133,10 @@ void loop() {
       digitalWrite(10, HIGH);
     } else if (data == '3') {
       digitalWrite(11, HIGH);
+    } else if (data == 'P') {
+      digitalWrite(12, HIGH);
+    } else if (data == 'H') {
+      digitalWrite(13, HIGH);
     }
 
     else if (data == '4') {
@@ -92,6 +159,10 @@ void loop() {
       digitalWrite(10, LOW);
     } else if (data == '$') {
       digitalWrite(11, LOW);
+    } else if (data == 'Q') {
+      digitalWrite(12, LOW);
+    } else if (data == 'M') {
+      digitalWrite(13, LOW);
     }
 
     else if (data == '%') {
@@ -114,7 +185,10 @@ void loop() {
       Serial.write(digitalRead(10));
     } else if (data == '.') {
       Serial.write(digitalRead(11));
+    } else if (data == '.') {
+      Serial.write(digitalRead(12));
+    } else if (data == 'R') {
+      Serial.write(digitalRead(13));
     }
-
-	}
+  }
 }
